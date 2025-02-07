c3344d1dca2abada2bf60f2f5883bcd6d45cd88c
mrclb - Marco <104036234+satisfyu@users.noreply.github.com>
2025-02-07 08:34:56 +0100

    feat(fix): Issue #433
    https://github.com/satisfyu/Let-s-Do-Hub/issues/433

--- common/src/main/java/net/satisfy/bloomingnature/core/block/SnowyFirLeavesBlock.java
+++ common/src/main/java/net/satisfy/bloomingnature/core/block/SnowyFirLeavesBlock.java
@@ -3,66 +3,86 @@ package net.satisfy.bloomingnature.core.block;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.tags.FluidTags;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SnowLayerBlock;
-import net.minecraft.world.level.block.SnowyDirtBlock;
+import net.minecraft.world.level.block.LeavesBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
-import net.minecraft.world.level.lighting.LightEngine;
 import net.satisfy.bloomingnature.core.registry.ObjectRegistry;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-public class SnowyFirLeavesBlock extends SnowyDirtBlock {
-    public static final BooleanProperty SNOWY = SnowyDirtBlock.SNOWY;
+public class SnowyFirLeavesBlock extends LeavesBlock {
+    public static final BooleanProperty SNOWY = BooleanProperty.create("snowy");
 
     public SnowyFirLeavesBlock(Properties properties) {
         super(properties);
-        this.registerDefaultState(this.stateDefinition.any().setValue(SNOWY, true));
-    }
-
-    private static boolean canBeGrass(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
-        BlockPos blockPos2 = blockPos.above();
-        BlockState blockState2 = levelReader.getBlockState(blockPos2);
-        if (blockState2.is(Blocks.SNOW) && blockState2.getValue(SnowLayerBlock.LAYERS) == 1) {
-            return true;
-        } else if (blockState2.getFluidState().getAmount() == 8) {
-            return false;
-        } else {
-            int i = LightEngine.getLightBlockInto(levelReader, blockState, blockPos, blockState2, blockPos2, Direction.UP, blockState2.getLightBlock(levelReader, blockPos2));
-            return i < levelReader.getMaxLightLevel();
-        }
-    }
-
-    private static boolean canPropagate(BlockState blockState, LevelReader levelReader, BlockPos blockPos) {
-        BlockPos blockPos2 = blockPos.above();
-        return canBeGrass(blockState, levelReader, blockPos) && !levelReader.getFluidState(blockPos2).is(FluidTags.WATER);
+        this.registerDefaultState(this.stateDefinition.any().setValue(DISTANCE, 7).setValue(PERSISTENT, false).setValue(SNOWY, false));
     }
 
     @Override
-    @Nullable
-    public BlockState getStateForPlacement(BlockPlaceContext ctx) {
-        return this.defaultBlockState().setValue(SNOWY, true);
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(SNOWY);
     }
 
-    @SuppressWarnings("deprecation")
-    public void randomTick(BlockState blockState, ServerLevel serverLevel, BlockPos blockPos, RandomSource randomSource) {
-        if (!canBeGrass(blockState, serverLevel, blockPos)) {
-            serverLevel.setBlockAndUpdate(blockPos, ObjectRegistry.FIR_LEAVES.get().defaultBlockState());
-        } else {
-            if (serverLevel.getMaxLocalRawBrightness(blockPos.above()) >= 9) {
-                BlockState blockState2 = this.defaultBlockState();
+    private static boolean shouldBeSnowy(LevelReader level, BlockPos pos) {
+        return level.getBlockState(pos.above()).is(Blocks.SNOW);
+    }
 
-                for (int i = 0; i < 4; ++i) {
-                    BlockPos blockPos2 = blockPos.offset(randomSource.nextInt(3) - 1, randomSource.nextInt(5) - 3, randomSource.nextInt(3) - 1);
-                    if (serverLevel.getBlockState(blockPos2).is(ObjectRegistry.FIR_LEAVES.get()) && canPropagate(blockState2, serverLevel, blockPos2)) {
-                        serverLevel.setBlockAndUpdate(blockPos2, blockState2.setValue(SNOWY, serverLevel.getBlockState(blockPos2.above()).is(Blocks.SNOW)));
-                    }
-                }
+    @Override
+    public @NotNull BlockState getStateForPlacement(BlockPlaceContext ctx) {
+        return this.defaultBlockState().setValue(SNOWY, shouldBeSnowy(ctx.getLevel(), ctx.getClickedPos()));
+    }
+
+    @Override
+    public void randomTick(BlockState state, ServerLevel world, BlockPos pos, RandomSource random) {
+        if (!state.getValue(PERSISTENT)) {
+            int distance = updateDistance(world, pos);
+            if (distance >= 7) {
+                world.removeBlock(pos, false);
+                return;
             }
+            world.setBlock(pos, state.setValue(DISTANCE, distance), 3);
+        }
+
+        boolean snowy = shouldBeSnowy(world, pos);
+        if (state.getValue(SNOWY) != snowy) {
+            world.setBlock(pos, state.setValue(SNOWY, snowy), 3);
         }
     }
+
+    @Override
+    public @NotNull BlockState updateShape(BlockState state, Direction direction, BlockState neighborState, LevelAccessor world, BlockPos pos, BlockPos neighborPos) {
+        if (direction == Direction.UP) {
+            boolean snowy = shouldBeSnowy(world, pos);
+            if (state.getValue(SNOWY) != snowy) {
+                return state.setValue(SNOWY, snowy);
+            }
+        }
+        return super.updateShape(state, direction, neighborState, world, pos, neighborPos);
+    }
+
+
+    private int updateDistance(Level world, BlockPos pos) {
+        int minDistance = 7;
+        for (BlockPos neighbor : BlockPos.betweenClosed(pos.offset(-1, -1, -1), pos.offset(1, 1, 1))) {
+            BlockState neighborState = world.getBlockState(neighbor);
+            if (neighborState.is(BlockTags.LOGS)) {
+                return 1;
+            }
+            if (neighborState.is(ObjectRegistry.FIR_LEAVES.get()) && neighborState.hasProperty(DISTANCE)) {
+                minDistance = Math.min(minDistance, neighborState.getValue(DISTANCE) + 1);
+            }
+        }
+        return minDistance;
+    }
+
 }
